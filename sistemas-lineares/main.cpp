b939159c21567400a2e83c480d2dc22276b90447
FredericoBresani <fbs4@cin.ufpe.br>
2024-06-07 21:52:34 -0300

    Finished PA = LU factorization

--- sistemas-lineares/main.cpp
+++ sistemas-lineares/main.cpp
@@ -51,29 +51,132 @@ subtraindo o lado direito dos dois lados, temos:
 |               6.3                                1.4                ...   (5.0 - (9.3 + 3.3 + ... + 5.0))  | | pk |   | 0 |
 --                                                                                                         --   -  -     --  
 
+Ax = b
+
+PA = LU
+
 
 */
 
 
 int main() {
     int n_empresas, aux_empresas = 0;
-    std::vector<std::vector<float>> matrix;
+    bool conditionMet = false;
     scanf("%i", &n_empresas);
+    std::vector<std::vector<float>> P(n_empresas);
+    std::vector<std::vector<float>> A;
+    std::vector<std::vector<float>> L;
+    std::vector<std::vector<float>> U;
+    std::vector<std::vector<float>> PA;
     while (aux_empresas < n_empresas) {
         float percentage;
         std::vector<float> line;
         for (int i = 0; i < n_empresas; i++) {
             scanf("%f", &percentage);
             line.push_back(percentage);
+            if (i == aux_empresas) {
+                P[aux_empresas].push_back(1);
+            } else {
+                P[aux_empresas].push_back(0);
+            }
         }
-        matrix.push_back(line);
+        A.push_back(line);
         aux_empresas++;
     }
-    for (auto line : matrix) {
-        for (auto n : line) {
-            std::cout << n << " ";
+    // calculate expenses and subtract from the incomes
+    for (int i = 0; i < A.size(); i++) {
+        if (A[i][i] == 0) {
+            for (int j = 0; j < A.size(); j++) {
+                if (A[j][i] != 0 && A[i][j] != 0) {
+                    std::vector<float> temp = P[j];
+                    P[j] = P[i];
+                    P[i] = temp;
+                }
+            }
         }
-        std::cout << "\n";
+    }
+    for (int i = 0; i < P.size(); i++) {
+        std::vector<float> line;
+        float value;
+        for (int t = 0; t < P.size(); t++) {
+            value = 0;
+            for (int j = 0; j < P.size(); j++) {
+                value = value + P[i][j]*A[j][t];
+            }
+            line.push_back(value);
+        }
+        U.push_back(line);
+    }
+    for (int i = 0; i < P.size(); i++) {
+        std::vector<float> line;
+        for (int j = 0; j < P.size(); j++) {
+            if (i == j) {
+                line.push_back(1);
+            } else {
+                line.push_back(0);
+            }
+        }
+        L.push_back(line);
+    }
+    PA = U;
+    for (int i = 0; i < U.size(); i++) {
+        if (U[i][i] != 0) { // a(esxalon_PA[i][i]) + b = 0 -> a = -b/(esxalon_PA[i][i]);
+            for (int j = i; j < U.size(); j++) {
+                if (j != i) {
+                    std::vector<float> temp;
+                    for (int t = 0; t < U[i].size(); t++) {
+                        temp.push_back(U[j][t] + U[i][t]*(-U[j][i]/U[i][i]));
+                    }
+                    U[j] = temp;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < U.size(); i++) {
+        std::vector<float> line(U.size());
+        for (int t = 0; t < U.size(); t++) {
+            if (t < i) {
+                if (PA[i][t] != 0) {
+                    float x = (PA[i][t] - line[t])/U[t][i];
+                    line[t] = x;
+                    for (int j = t + 1; j < line.size(); j++) {
+                        line[j] = line[j] + U[t][j]*x;
+                    }
+                } else {
+                    line[t] = 0;
+                }
+            } else if (t == i) {
+                line[t] = 1;
+            } else {
+                line[t] = 0;
+            }
+        }
+        L[i] = line;
+    }
+    for (int i = 0; i < P.size(); i++) {
+        std::cout << "| ";
+        for (int j = 0; j < P[i].size(); j++) {
+            std::cout << P[i][j] << " ";
+        }
+        std::cout << "| | ";
+        for (int j = 0; j < A[i].size(); j++) {
+            std::cout << A[i][j] << " ";
+        }
+
+        if (i == P.size()/2) {
+            std::cout << "| = | ";
+        } else {
+            std::cout << "|   | ";
+        }
+        
+        for (int j = 0; j < L.size(); j++) {
+            std::cout << L[i][j] << " ";
+        }
+        std::cout << "| | ";
+        for (int j = 0; j < U.size(); j++) {
+            std::cout << U[i][j] << " ";
+        }
+        std::cout << "|\n";
     }
     return 0;
 }
