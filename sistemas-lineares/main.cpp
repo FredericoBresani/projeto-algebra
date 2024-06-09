817c04e5e9a55ded83184fae01a80c87cda06868
FredericoBresani <fbs4@cin.ufpe.br>
2024-06-09 20:41:34 -0300

    Gains subtraced from left side matrix and regular expressions added to calculate the results vector

--- sistemas-lineares/main.cpp
+++ sistemas-lineares/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 /* 
  Empresa   |                                             Cada linha representa as comrpras de um empresa
@@ -58,6 +59,17 @@ PA = LU
 
 */
 
+bool isNumber(char c) {
+    return (c == '0' || c == '1' ||c == '2' ||c == '3' ||c == '4' ||c == '5' ||c == '6' ||c == '7' ||c == '8' || c == '9' || c == '.');
+}
+
+class Tuple {
+    public:
+        char c;
+        int index;
+        Tuple(char cc, int i): c(cc), index(i) {}
+};
+
 
 int main() {
     int n_empresas, aux_empresas = 0;
@@ -75,9 +87,9 @@ int main() {
             scanf("%f", &percentage);
             line.push_back(percentage);
             if (i == aux_empresas) {
-                P[aux_empresas].push_back(1);
+                P[aux_empresas].push_back(1.0);
             } else {
-                P[aux_empresas].push_back(0);
+                P[aux_empresas].push_back(0.0);
             }
         }
         A.push_back(line);
@@ -85,9 +97,21 @@ int main() {
     }
     // calculate expenses and subtract from the incomes
     for (int i = 0; i < A.size(); i++) {
-        if (A[i][i] == 0) {
+        float gain = 0.0;
+        for (int t = 0; t < A.size(); t++) {
+            gain += A[t][i];
+        }
+        A[i][i] = A[i][i] - gain;
+    }
+    for (int i = 0; i < A.size(); i++) {
+        for (int j = 0; j < A.size(); j++) {
+            A[i][j] = -A[i][j];
+        }
+    }
+    for (int i = 0; i < A.size(); i++) {
+        if (A[i][i] == 0.0) {
             for (int j = 0; j < A.size(); j++) {
-                if (A[j][i] != 0 && A[i][j] != 0) {
+                if (A[j][i] != 0.0 && A[i][j] != 0.0) {
                     std::vector<float> temp = P[j];
                     P[j] = P[i];
                     P[i] = temp;
@@ -99,7 +123,7 @@ int main() {
         std::vector<float> line;
         float value;
         for (int t = 0; t < P.size(); t++) {
-            value = 0;
+            value = 0.0;
             for (int j = 0; j < P.size(); j++) {
                 value = value + P[i][j]*A[j][t];
             }
@@ -111,16 +135,16 @@ int main() {
         std::vector<float> line;
         for (int j = 0; j < P.size(); j++) {
             if (i == j) {
-                line.push_back(1);
+                line.push_back(1.0);
             } else {
-                line.push_back(0);
+                line.push_back(0.0);
             }
         }
         L.push_back(line);
     }
     PA = U;
     for (int i = 0; i < U.size(); i++) {
-        if (U[i][i] != 0) { // a(esxalon_PA[i][i]) + b = 0 -> a = -b/(esxalon_PA[i][i]);
+        if (U[i][i] != 0.0) { // a(esxalon_PA[i][i]) + b = 0 -> a = -b/(esxalon_PA[i][i]);
             for (int j = i; j < U.size(); j++) {
                 if (j != i) {
                     std::vector<float> temp;
@@ -136,19 +160,19 @@ int main() {
         std::vector<float> line(U.size());
         for (int t = 0; t < U.size(); t++) {
             if (t < i) {
-                if (PA[i][t] != 0) {
-                    float x = (PA[i][t] - line[t])/U[t][i];
+                if (PA[i][t] != 0.0) {
+                    float x = (PA[i][t] - line[t])/U[t][t];
                     line[t] = x;
                     for (int j = t + 1; j < line.size(); j++) {
                         line[j] = line[j] + U[t][j]*x;
                     }
                 } else {
-                    line[t] = 0;
+                    line[t] = 0.0;
                 }
             } else if (t == i) {
-                line[t] = 1;
+                line[t] = 1.0;
             } else {
-                line[t] = 0;
+                line[t] = 0.0;
             }
         }
         L[i] = line;
@@ -156,11 +180,11 @@ int main() {
     for (int i = 0; i < P.size(); i++) {
         std::cout << "| ";
         for (int j = 0; j < P[i].size(); j++) {
-            std::cout << P[i][j] << " ";
+            printf("%.1f ", P[i][j]);
         }
         std::cout << "| | ";
         for (int j = 0; j < A[i].size(); j++) {
-            std::cout << A[i][j] << " ";
+            printf("%.1f ", A[i][j]);
         }
 
         if (i == P.size()/2) {
@@ -170,13 +194,109 @@ int main() {
         }
         
         for (int j = 0; j < L.size(); j++) {
-            std::cout << L[i][j] << " ";
+            printf("%.1f ", L[i][j]);
         }
         std::cout << "| | ";
         for (int j = 0; j < U.size(); j++) {
-            std::cout << U[i][j] << " ";
+            printf("%.1f ", U[i][j]);
         }
         std::cout << "|\n";
     }
+    
+    /*
+    for (int i = U.size() - 1; i >= 0; i--) {
+        bool lineOfZeros = true;
+        bool lineDependsOnVariable = false;
+        for (int j = 0; j < U.size(); j++) {
+            if (U[i][j] != 0) {
+                lineOfZeros = false;
+                for (int t = 0; t < solution.size(); t++) {
+                    
+                }
+            }
+        }
+        if (lineOfZeros) {
+           
+        }
+    }
+    */
+    std::vector<std::string> solutions(n_empresas);
+    std::vector<std::string> variables(n_empresas);
+    for (int i = U.size() - 1; i >= 0; i--) {
+        float value;
+        solutions[i] = "k"+std::to_string(i)+"=(";
+        for (int j = 0; j < U.size(); j++) {
+            if (j != i) {
+                if (U[i][j] != 0) {
+                    solutions[i] = solutions[i] + std::to_string(-U[i][j]) + "*k" + std::to_string(j) + "+";
+                } 
+            }
+        }
+        solutions[i] = solutions[i] + ")/" + std::to_string(U[i][i]);
+    }
+    for (int i = solutions.size() - 1; i >= 0; i--) {
+        int variableIndex = -1;
+        int peek = 1;
+        int firstParentheses = 0;
+        for (int j = 1; j < solutions[i].length(); j++) {
+            while (isNumber(solutions[i].at(peek))) {
+                peek++;
+            }
+            if (peek != j) {
+                std::string n = solutions[i].substr(j, peek - 1);
+                variableIndex = std::stoi(n);
+                j = peek;
+            }
+
+            while (solutions[i].at(peek) != '(') {
+                firstParentheses = j;
+                peek++;
+            } 
+            peek++;
+            j = peek;
+            bool insideBracket = solutions[i].at(peek) != ')';
+
+            while (solutions[i].at(peek) != '/') {
+                peek++;
+            }
+            peek++;
+            j = peek;
+
+            while (isNumber(solutions[i].at(peek)) && peek < solutions[i].size() - 1) {
+                peek++;
+            }
+
+            if (j != peek) {
+                std::string n = solutions[i].substr(j, peek);
+                float devisor = std::stof(n);
+                j = peek;
+                if (devisor == 0) {
+                    if (insideBracket) {
+                        variables[variableIndex] = "nosolution";
+                    } else {
+                        if (variableIndex == solutions.size() / 2) {
+                            variables[variableIndex] = "|k"+std::to_string(variableIndex)+"| = | " + "variable |";
+                        } else {
+                            variables[variableIndex] = "|k"+std::to_string(variableIndex)+"|   | " + "variable |";
+                        }
+                        
+                    }
+                } else {
+                    if (variableIndex == solutions.size() / 2) {
+                        variables[variableIndex] = "|k"+std::to_string(variableIndex)+"| = | " + solutions[i].substr(firstParentheses + 1, solutions[i].size() - 1) + " |";
+
+                    } else {
+                        variables[variableIndex] = "|k"+std::to_string(variableIndex)+"|   | " + solutions[i].substr(firstParentheses + 1, solutions[i].size() - 1) + " |";
+
+                    }
+                }
+            }
+        }
+    }
+    std::cout<< "\n\n\n";
+    for (int i = 0; i < variables.size(); i++) {
+        std::cout << variables[i] << "\n";
+    }
     return 0;
 }
+// 
